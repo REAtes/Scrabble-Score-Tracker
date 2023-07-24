60ea78409d287a09f27b60a18e472bfec3b4aff1
Emre Ates <125511820+REAtes@users.noreply.github.com>
2023-07-24 16:00:38 +0200

    Scrabble.py

--- Scrabble.py
+++ Scrabble.py
@@ -1,3 +1,7 @@
+import streamlit as st
+gatherUsageStats = false
+
+
 ###################### Scrabble ######################
 # In this project, you will process some data from a group of friends playing scrabble. You will use dictionaries to organize players, words, and points.
 # We have provided you with two lists, letters and points. We would like to combine these two into a dictionary that would map a letter to its point value. 
